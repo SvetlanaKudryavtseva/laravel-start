df7e4169360d923e538f5d69d89ae7c828f517c7
Svetlana Kudryavtseva <springinmypalms@gmail.com>
2021-10-10 13:16:06 +0300

    mysql setup, Model Posts -mc is created

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.layout-main')
+@section('title', 'My posts')
+@section('content')
+    <div class="btn-group mb-4" role="group" >
+        Category 1</div>
+    <div class="category">Category 2</div>
+@endsection
